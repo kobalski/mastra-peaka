e59e6346bdbe0865e402698a5bb381fb2e5474bd
İbrahim Gurses <igrses9@gmail.com>
2025-06-30 20:49:43 +0300

    WIP

---
+++ src/mastra/agents/weather-agent.ts
@@ -0,0 +1,21 @@
+import { openai } from "@ai-sdk/openai";
+import { Agent } from "@mastra/core/agent";
+import { Memory } from "@mastra/memory";
+import { LibSQLStore } from "@mastra/libsql";
+import { getSQLRuleSet, mcp } from "./mcp";
+
+export const weatherAgent = new Agent({
+  name: "Weather Agent",
+  instructions:
+    `
+      You are a helpful weather assistant that does text2sql
+
+` + (await getSQLRuleSet()),
+  model: openai("gpt-4o-mini"),
+  tools: await mcp.getTools(),
+  memory: new Memory({
+    storage: new LibSQLStore({
+      url: "file:../mastra.db", // path is relative to the .mastra/output directory
+    }),
+  }),
+});
